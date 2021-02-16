147c06c690740f658a341c0a0a896e9a8e306391
Everton Murilo Vieira <emuvs@pointel.com.br>
2021-02-15 21:43:41 -0300

    prototype development

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [got, setGot] = useState("nothing");
+
+  axios
+    .get("http://localhost:36549/ping")
+    .then((res) => {
+      setGot(res.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Qinpel</h1>
+        <h2>Quick Interface for Pointel</h2>
+        <h3>{got}</h3>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
